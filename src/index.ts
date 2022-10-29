35c3f062ce06610b4694b2b0dacc51b056ae8604
Kushnir Maxim <maxbasta2006@gmail.com>
2022-10-29 02:32:31 +0200

    change library version and prompt service and types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import { PromptService } from './core/prompt/prompt.service';
+
 export class App {
-    sayHello(): void {
-        console.log("Hi");
-    };
+	async run() {
+		const res = await (new PromptService()).input<number>('Число', 'number');
+		console.log(res);
+	}
 }
 
 const app = new App();
-app.sayHello();
+app.run();
